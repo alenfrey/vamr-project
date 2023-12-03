bde9a36fb784e85d47b057d2545e607c8c26bb97
alenfrey <datenmarmelade@gmail.com>
2023-12-03 23:18:51 +0100

    add dataset loader to main loop

--- constants.py
+++ constants.py
@@ -5,9 +5,15 @@ PROJECT_ROOT_DIR_PATH = Path(__file__).parent
 
 # subdirectories relative to project root directory
 DATA_DIR_PATH = PROJECT_ROOT_DIR_PATH / "data"
+
 # its good practice to have a raw data folder that is kept as originally downloaded
 RAW_DATA_DIR_PATH = DATA_DIR_PATH / "raw"
-# folder for processed data
 PROCESSED_DATA_DIR_PATH = DATA_DIR_PATH / "processed"
+
+# dataset paths
+PARKING_DATA_DIR_PATH = RAW_DATA_DIR_PATH / "parking"
+KITTI_DATA_DIR_PATH = RAW_DATA_DIR_PATH / "kitti"
+MALAGA_DATA_DIR_PATH = RAW_DATA_DIR_PATH / "malaga"
+
 
 REPORT_DIR_PATH = PROJECT_ROOT_DIR_PATH / "reports"
--- main.py
+++ main.py
@@ -2,6 +2,32 @@ from constants import *
 from src.camera_calibration import *
 from src.initialization import *
 from src.utils import *
+from src.data_loaders import *
 
-# ToDo implement main loop for VO, calling the modules from src folder
+dataset_loader = ParkingDataLoader(PARKING_DATA_DIR_PATH)
 
+# example of loading the parking dataset with specific initialization frames
+init_frames = [0, 2]  # e.g., frame 1 and frame 3 for initialization
+dataset_loader = ParkingDataLoader(
+    PARKING_DATA_DIR_PATH, init_frame_indices=init_frames
+)
+
+camera_intrinsics = dataset_loader.load_camera_intrinsics()
+initialization_images = dataset_loader.get_initialization_frames()
+
+# Main VO Loop, streamed from the dataset
+dataset_loader.current_image_index = len(
+    initialization_images
+)  # skip initialization frames
+
+for idx, (image, pose) in enumerate(dataset_loader):
+    # TODO: implement the main VO loop here
+
+    cv2.imshow("Image Stream", image)
+
+    # Wait for 30ms or until a key is pressed; if 'q' is pressed, break the loop
+    if cv2.waitKey(30) & 0xFF == ord("q"):
+        break
+
+# When everything is done, release the OpenCV window
+cv2.destroyAllWindows()
