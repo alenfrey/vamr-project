597f6148735a84bb73eeaa7f8e91661250ccc5f3
Noah Mamié <noah.mamie@uzh.ch>
2023-12-08 16:16:11 +0100

    implemented the calculation of the initial keypoints

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 
 from constants import *
 from src.camera_calibration import *
-from src.initialization import initialize_vo, ensure_grayscale
+from src.initialization import initialize_vo, ensure_grayscale, initialize_keypoints_harris, initialize_keypoints_sift
 from src.utils import *
 from src.data_loaders import ParkingDataLoader
 from src.continuous_operation import (
@@ -28,7 +28,21 @@ camera_intrinsics = dataset_loader.load_camera_intrinsics()
 initialization_images = dataset_loader.get_initialization_frames()
 R, t = initialize_vo(initialization_images, camera_intrinsics)
 prev_image = initialization_images[-1]
-# TODO: calculate keypoints of prev_image to use in first main loop iteration
+# initialize keypoints
+# image_1, keypoints_1, descriptors_1 = initialize_keypoints(prev_image)
+# initialize parameter for Harris (TODO: tune)
+corner_patch_size = 9
+kernel_size = 3
+harris_kappa = 0.08
+num_keypoints = 200
+nonmaximum_supression_radius = 8
+descriptor_radius = 9
+match_lambda = 4
+
+# run Harris on first frame
+keypoints_1 = initialize_keypoints_harris(prev_image, corner_patch_size, kernel_size, harris_kappa)
+
+prev_keypoints = None
 
 for iteration, (curr_image, pose, image_index) in enumerate(dataset_loader):
     print(f"Processing frame {image_index}...")
@@ -37,7 +51,9 @@ for iteration, (curr_image, pose, image_index) in enumerate(dataset_loader):
 
     # Keypoint Association
     # Use KLT or another method to find keypoints in the current frame and associate them with previous frame's keypoints.
-    associate_keypoints(curr_image, prev_image, ...)
+    # if prev_keypoints is None:
+    #     prev_keypoints = keypoints_1
+    associate_keypoints(curr_image, prev_image, prev_keypoints)
 
     # Pose Estimation
     # Use the associated keypoints to estimate the current camera pose.
--- src/initialization.py
+++ src/initialization.py
@@ -53,3 +53,61 @@ def initialize_vo(initialization_images, camera_intrinsics):
     # TODO: initialize landmarks (3D points) (triangulatoin)
 
     return R, t  # Return initial rotation and translation matrices
+
+@timer
+def initialize_keypoints_harris(first_image, block_size, ksize, k):
+    # Harris corner detectore for initializing keypoints
+    img = first_image
+    # gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    gray = img
+
+    dst = cv2.cornerHarris(gray, block_size, ksize, k)
+    dst = cv2.dilate(dst,None)
+    ret, dst = cv2.threshold(dst,0.01*dst.max(),255,0)
+    dst = np.uint8(dst)
+    # find centroids
+    ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+    # define the criteria to stop and refine the corners
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+    corners = cv2.cornerSubPix(gray,np.float32(centroids),(5,5),(-1,-1),criteria)
+
+    # Now draw them
+    res = np.hstack((centroids,corners))
+    res = np.int0(res)
+
+    # Convert the grayscale image to a BGR color image
+    img_color = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    # Iterate through each point and draw a cross
+    for i in range(len(res)):
+        y1, x1 = int(res[i, 1]), int(res[i, 0])
+        y2, x2 = int(res[i, 3]), int(res[i, 2])
+
+        # Draw yellow crosses
+        draw_cross(img_color, (x1, y1))
+        draw_cross(img_color, (x2, y2))
+
+    # Display the result
+    cv2.imshow('Image with Yellow Crosses', img_color)
+    return corners
+
+@timer
+def initialize_keypoints_sift(first_image):
+    # calcualte SIFT keypoints and descriptors
+    sift = cv2.SIFT_create()
+
+    # detect keypoints
+    keypoints_1, descriptors_1 = sift.detectAndCompute(first_image, None)
+
+    # Draw keypoints for visualization
+    image1_with_keypoints = cv2.drawKeypoints(first_image, keypoints_1, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    # Display the image with keypoints
+    cv2.imshow('Keypoints in the First Image', image1_with_keypoints)
+
+    return image1_with_keypoints, keypoints_1, descriptors_1
+
+# Function to draw a cross
+def draw_cross(img, center, size=5, color=(0, 255, 255)):
+    # Draw two lines intersecting at the center
+    cv2.line(img, (center[0] - size, center[1]), (center[0] + size, center[1]), color, 1)
+    cv2.line(img, (center[0], center[1] - size), (center[0], center[1] + size), color, 1)
